8806c9e80568d2c260263dfd0612bc9d780c7a64
enilu <eniluzt@qq.com>
2019-11-21 17:29:09 +0800

    Change 微信小程序-调整eslint规则

--- linjiashop-wxapp/.eslintrc.js
+++ linjiashop-wxapp/.eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    "space-before-function-paren": 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   },

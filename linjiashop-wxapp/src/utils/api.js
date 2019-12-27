f90552e6c077fdce2a38151f4ade6d5aae70c801
enilu <eniluzt@qq.com>
2019-12-27 23:39:22 +0800

    Change host地址最后添加/避免接口请求的时候没有以/开头导致的请求失败

--- linjiashop-wxapp/src/utils/api.js
+++ linjiashop-wxapp/src/utils/api.js
@@ -2,7 +2,7 @@ import store from './store'
 import utils from '@/utils/index.js'
 
 // const host = 'http://linjiashop-mobile-api.microapp.store/'
-const host = 'http://localhost:8081'
+const host = 'http://localhost:8081/'
 export default ($wx) => {
   let reqHeader = {
     'content-type': 'application/json'

ff5a04aac07aa769a58e4fe54710dbe51e678457
enilu <eniluzt@qq.com>
2020-03-21 22:38:00 +0800

    Fix 微信支付

--- linjiashop-mobile/src/view/order/payment/payment.js
+++ linjiashop-mobile/src/view/order/payment/payment.js
@@ -52,7 +52,7 @@ export default {
                     {
                         appId: data.appId,
                         nonceStr: data.nonceStr,
-                        package: data.package,
+                        package: data.packageValue,
                         paySign: data.paySign,
                         signType: data.signType,
                         timeStamp: data.timeStamp
@@ -70,7 +70,7 @@ export default {
             let timestamp = data.timeStamp
             let nonceStr = data.nonceStr
             let signature = data.signature
-            let packages = data.packageValue
+            let packageValue = data.package
             let paySign = data.paySign
             let signType = data.signType
             console.log('发起微信支付')
@@ -87,7 +87,7 @@ export default {
                 wx.chooseWXPay({
                     timestamp: timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                     nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
-                    package: packages, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+                    package: packageValue, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
                     signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
                     paySign: paySign, // 支付签名
                     success: function (res) {

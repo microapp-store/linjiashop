a6607ee7e1ccf8414bc323eb473e868c89745ce7
enilu <eniluzt@qq.com>
2020-03-21 12:08:20 +0800

    Change 前端显示微信支付失败提醒消息

--- linjiashop-mobile/src/view/order/payment/payment.js
+++ linjiashop-mobile/src/view/order/payment/payment.js
@@ -60,7 +60,7 @@ export default {
 
                 )
             }).catch(err =>{
-                Toast('系统异常，青稍后再试')
+                Toast(err)
             })
 
         },
@@ -103,6 +103,7 @@ export default {
                 })
             })
             wx.error(function (res) {
+                console.log('微信支付失败',res)
                 // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
                 /*alert("config信息验证失败")*/
             })

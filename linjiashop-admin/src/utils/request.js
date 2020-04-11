0e246a23dd2e803c0d6dfac9b1683e3323598aa0
enilu <eniluzt@qq.com>
2020-04-11 22:53:36 +0800

    Fix 静态界面段配置问题导致无法跨域

--- linjiashop-admin/src/utils/request.js
+++ linjiashop-admin/src/utils/request.js
@@ -6,7 +6,7 @@ import router from '@/router'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: false, // send cookies when cross-domain requests
   timeout: 30000 // request timeout
 })
 
--- linjiashop-mobile/src/utils/request.js
+++ linjiashop-mobile/src/utils/request.js
@@ -7,7 +7,7 @@ import { Toast } from 'vant'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  withCredentials: true, // send cookies when cross-domain requests
+  withCredentials: false, // send cookies when cross-domain requests
   timeout: 15000 // request timeout
 })
 

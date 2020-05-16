cb0f99a5115ab3a9b0efa7ecf2d70f0eb079124b
enilu <eniluzt@qq.com>
2020-05-17 00:50:26 +0800

    Issue #I1HEM7 优化获取用户微信信息的逻辑

--- linjiashop-core/src/main/java/cn/enilu/flash/bean/exception/ApplicationExceptionEnum.java
+++ linjiashop-core/src/main/java/cn/enilu/flash/bean/exception/ApplicationExceptionEnum.java
@@ -24,6 +24,7 @@ public enum ApplicationExceptionEnum implements ServiceExceptionEnum{
 	 */
 	REQUEST_NULL(400, "请求有错误"),
 	SERVER_ERROR(500, "服务异常，请联系管理员"),
+	WECHAT_BIND_ANOTHER(500, "该手机号已经绑定其他微信号"),
 	REQUEST_TOO_MANY(500, "请求频繁，请稍后重试"),
 	TASK_CONFIG_ERROR(500, "定时任务配置错误");
 

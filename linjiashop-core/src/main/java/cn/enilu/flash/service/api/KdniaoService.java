fbd4a129adc099cee2932029e1634a860e7a59fb
enilu <eniluzt@qq.com>
2020-06-06 00:34:51 +0800

    Change 去掉测试代码

--- linjiashop-core/src/main/java/cn/enilu/flash/service/api/KdniaoService.java
+++ linjiashop-core/src/main/java/cn/enilu/flash/service/api/KdniaoService.java
@@ -5,7 +5,10 @@ import cn.enilu.flash.bean.exception.ApplicationException;
 import cn.enilu.flash.bean.exception.ApplicationExceptionEnum;
 import cn.enilu.flash.cache.CacheDao;
 import cn.enilu.flash.service.system.CfgService;
-import cn.enilu.flash.utils.*;
+import cn.enilu.flash.utils.Base64Util;
+import cn.enilu.flash.utils.JsonUtil;
+import cn.enilu.flash.utils.MD5;
+import cn.enilu.flash.utils.Maps;
 import org.nutz.http.Http;
 import org.nutz.http.Response;
 import org.slf4j.Logger;
@@ -14,9 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.net.URLEncoder;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.Map;
 
 /**
@@ -44,24 +44,6 @@ public class KdniaoService {
 
         KdniaoResponse kdniaoResponse = cacheDao.hget(CacheDao.HOUR, orderCode, KdniaoResponse.class);
         if (kdniaoResponse != null) {
-            return kdniaoResponse;
-        }
-        //todo 测试数据
-        String txt = "{\"LogisticCode\":\"YT45782342348\",\"ShipperCode\":\"YTO\",\"Traces\":[{\"AcceptStation\":\"【上海市奉贤区南桥公司】 已收件 取件人: 仰琪锋 (02167190359)\",\"AcceptTime\":\"2020-05-25 15:59:59\"},{\"AcceptStation\":\"【宁波转运中心公司】 已收入\",\"AcceptTime\":\"2020-05-26 03:01:31\"},{\"AcceptStation\":\"【宁波转运中心】 已发出 下一站 【上海市浦东新区博兴公司】\",\"AcceptTime\":\"2020-05-26 03:22:31\"},{\"AcceptStation\":\"【浙江省舟山市公司】 已收入\",\"AcceptTime\":\"2020-05-26 06:31:51\"},{\"AcceptStation\":\"快件已由上海市莱阳路431号店菜鸟驿站代收，请及时取件，如有疑问请联系13262707273\",\"AcceptTime\":\"2020-05-28 11:33:33\"},{\"AcceptStation\":\"客户签收人: 已签收，签收人凭取货码签收。 已签收  感谢使用圆通速递，期待再次为您服务 如有疑问请联系：13381798120，投诉电话：18521102150\",\"AcceptTime\":\"2020-05-28 18:59:55\"}],\"State\":\"3\",\"EBusinessID\":\"1627883\",\"Success\":true}\n";
-        kdniaoResponse = JsonUtil.fromJson(KdniaoResponse.class,txt);
-        //按照日期进行倒叙排序
-        Collections.sort(kdniaoResponse.getTraces(), new Comparator<KdniaoResponse.Trace>() {
-            @Override
-            public int compare(KdniaoResponse.Trace t1, KdniaoResponse.Trace t2) {
-                Date d1 = DateUtil.parseTime(t1.getAcceptTime());
-                Date d2 = DateUtil.parseTime(t2.getAcceptTime());
-                return d1.getTime()<d2.getTime()?1:-1;
-            }
-        });
-
-        //todo 测试
-        if(1==1){
-            cacheDao.hset(CacheDao.HOUR,orderCode,kdniaoResponse);
             return kdniaoResponse;
         }
         String url = cfgService.getCfgValue(CfgKey.API_KDNIAO_URL);

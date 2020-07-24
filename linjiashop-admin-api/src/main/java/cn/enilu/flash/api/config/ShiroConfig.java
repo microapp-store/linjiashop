49731c835be085876d1984ce42eb2a03a245844b
东平王北星 <364173778@qq.com>
2020-07-24 16:36:14 +0800

    修复shiro引起的事物失效,缓存失效问题. 修复删除菜单和关联表没有事物的问题

--- linjiashop-admin-api/src/main/java/cn/enilu/flash/api/config/ShiroConfig.java
+++ linjiashop-admin-api/src/main/java/cn/enilu/flash/api/config/ShiroConfig.java
@@ -14,6 +14,7 @@ import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreato
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
 
 import javax.servlet.Filter;
 import java.util.LinkedHashMap;
@@ -26,7 +27,7 @@ import java.util.Map;
 @Configuration
 public class ShiroConfig {
     @Bean("securityManager")
-    public DefaultWebSecurityManager getManager(ApiRealm realm) {
+    public DefaultWebSecurityManager getManager(@Lazy ApiRealm realm) {
         DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
         // 使用自己的realm
         manager.setRealm(realm);
